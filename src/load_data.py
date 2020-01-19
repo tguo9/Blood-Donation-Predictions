875a8418382b5d645a8b4d9f5859e1e4907326c1
Lise Braaten <lisebraaten@gmail.com>
2020-01-18 20:23:39 -0800

    finished script to load data

--- src/load_data.py
+++ src/load_data.py
@@ -1,4 +1,4 @@
-# authors: DSCI 522 Group 203 
+# authors: DSCI 522 Group 305
 # date: 2020-01-18
 
 """
@@ -8,8 +8,8 @@ Takes a URL and a local file path as arguments
 Usage: load_data.py --data_url=<data_url> --local_path=<local_path>
 
 Options:
---data_url=<data_url>         URL of the data 
---local_path=<local_path>     Local path you want the csv file to be written to
+--data_url=<data_url>      URL of the data 
+--local_path=<local_path>  Local path you want the csv file to be written to (include file name)
 """
 
 import pandas as pd
@@ -18,6 +18,7 @@ from docopt import docopt
 opt = docopt(__doc__)
 
 def main(data_url, local_path):
+    # in console type: python load_data.py --data_url 'https://raw.githubusercontent.com/shivang98/Social-Network-ads-Boost/master/Social_Network_Ads.csv' --local_path ../data/raw_data.csv
     data = pd.read_csv(data_url)
     data.to_csv(local_path)
 

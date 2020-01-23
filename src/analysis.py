f40a36352bc963ff7cf13cb328b54f8f17585238
xinwen01 <xinwenwang623@gmail.com>
2020-01-23 14:24:28 -0800

    change test as validation

--- src/analysis.py
+++ src/analysis.py
@@ -4,11 +4,10 @@
 """
 This script read in the data processed by pre-process.py and apply decision tree algorithm
 
-Usage: src/analysis.py --train_data=<train_data> --test_data=<test_data> --local_path=<local_path>
+Usage: src/analysis.py --train_data=<train_data>  --local_path=<local_path>
 
 Options:
 --train_data=<train_data>  Local path to the training data csv file (include file name)
---test_data=<test_data>  Local path to the training data csv file (include file name)
 --local_path=<local_path>  Local path to folder you want the analysis files to be written to 
 """
 
@@ -16,29 +15,30 @@ import pandas as pd
 from docopt import docopt
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
 
 opt = docopt(__doc__)
 
-def main(train_data, test_data, local_path):
-    # in console type: python src/analysis.py --train_data=./data/processed/train_data.csv --test_data=./data/processed/test_data.csv --local_path=./results
+def main(train_data, local_path):
+    # in console type: python src/analysis.py --train_data=./data/processed/train_data.csv  --local_path=./results
     
     #convert to pandas data frame
     train_df = pd.read_csv(train_data)
-    test_df = pd.read_csv(test_data)
-    X_train = train_df.drop(columns='Class')
-    y_train = train_df[['Class']]
-    X_test = test_df.drop(columns='Class')
-    y_test = test_df[['Class']]
+    X = train_df.drop(columns='Class')
+    y = train_df[['Class']]
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state = 123)
+    
 
     #fit and predict using decision tree classifer
     model = DecisionTreeClassifier()
     model.fit(X_train, y_train)
-    model_test_score = pd.DataFrame(cross_val_score(model, X_test, y_test, cv=10))
-    
+    model_score = pd.DataFrame(cross_val_score(model, X_valid, y_valid, cv=10))
+    model_score.columns=['test_score']
+    model_score['train_score'] = cross_val_score(model, X_train, y_train, cv=10)
 
     #write to csv
-    model_test_score.to_csv("%s/analysis_result.csv" % local_path)
+    model_score.to_csv("%s/analysis_result.csv" % local_path)
     
   
 if __name__ == "__main__":
-    main(opt["--train_data"], opt["--test_data"],opt["--local_path"])
+    main(opt["--train_data"],opt["--local_path"])
